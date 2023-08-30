31f9ef8deb814d6d41e1c24a12aaca84d61cece0
rafakook <142992343+rafakook@users.noreply.github.com>
2023-08-30 11:51:24 -0300

    criacao do menu inicial
    utilizar if para condicoes

---
+++ projeto do sr.joao/projeto do sr.joao/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_do_sr.joao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            string loop = "S";
+
+            while (loop == "S")
+            {
+
+
+                Console.WriteLine("1- cadastrar novos produtos");
+                Console.WriteLine("2- realizar vendas de perodutos");
+                Console.WriteLine("3- gerar relatorio produtos");
+                menu = int.Parse(Console.ReadLine());
+
+                if (menu == 1)
+                {
+                    Console.WriteLine("voce escolheu cadastrar um produto");
+                }
+                else if (menu == 2)
+                {
+                    Console.WriteLine("voce escolheu vender um produto");
+                }
+                else if (menu == 3)
+                {
+                    Console.WriteLine("voce escolheu gerar um relatorio dos produtos");
+                }
+                else
+                {
+    
+                    Console.WriteLine("informe uma informacao valida");
+                    
+                }
+                Console.Write("deseja continuar no sistema (s/n)");
+                loop = Console.ReadLine();
+
+            }
+
+            {
+                Console.ReadKey();
+            }
+
+         
+
+            
+            
+            
+            
+            
+           
+           
+            
+
+
+
+        }
+    }
+}
